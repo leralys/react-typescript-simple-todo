74bd46a223eb8369afb81fb92fbcc1af9db043c5
leralys <valerialysko@gmail.com>
2022-05-08 00:32:10 +0300

    basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
+import { Todo } from './todo.model';
 
 const App: React.FC = () => {
-  const todos = [{ id: 't1', text: 'Finish the course' }];
+  // const [todos, setTodos] = useState<{ id: string; text: string }[]>([]);
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
+    console.log(todos);
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
   return (
     <div className='App'>
-      {/* a component that adds todos */}
-      <TodoList items={todos} />
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
     </div>
   );
 };
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 
 interface TodoListProps {
   items: { id: string; text: string }[];
+  onDeleteTodo: (id: string) => void;
 }
 
 const TodoList: React.FC<TodoListProps> = (props) => {
   return (
     <ul>
       {props.items.map((todo) => (
-        <li key={todo.id}>{todo.text}</li>
+        <li key={todo.id}>
+          <span>{todo.text}</span>
+          {/* we expect to get todoId as a parameter, for this we use bind(), we do not bind the 'this', so the 1st param is null,
+          the second argument  we pass to bind() will be the first parameter received in the onDeleteTodo function, and that is the todoId*/}
+          <button onClick={props.onDeleteTodo.bind(null, todo.id)}>
+            DELETE
+          </button>
+        </li>
       ))}
     </ul>
   );
