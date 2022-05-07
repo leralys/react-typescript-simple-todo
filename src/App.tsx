e01e2cc51223a53d54ac9a7c1dfbc2b5dca9ec50
leralys <valerialysko@gmail.com>
2022-05-07 21:03:30 +0300

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import TodoList from './components/TodoList';
 
 const App: React.FC = () => {
-  return <div className='App'></div>;
+  const todos = [{ id: 't1', text: 'Finish the course' }];
+  return (
+    <div className='App'>
+      {/* a component that adds todos */}
+      <TodoList items={todos} />
+    </div>
+  );
 };
 
 export default App;
